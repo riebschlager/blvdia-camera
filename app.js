daed3e9994d7f10b1b63f70696494456ff7c4e59
Chris Riebschlager <riebschlager@gmail.com>
2015-04-22 23:49:30 -0500

    first

---
+++ app.js
@@ -0,0 +1,41 @@
+var config = require('./config');
+var childProcess = require('child_process');
+var imgur = require('imgur');
+var io = require('socket.io-client');
+var socket = io.connect('blvdia.herokuapp.com', {
+    port: 80
+});
+
+socket.on('shutter', function(msg) {
+    if (msg.cameraId === config.cameraId) {
+        start(msg.clientId);
+    }
+});
+
+var start = function(clientId) {
+    var snapIndex = 0;
+    var exec = childProcess.exec('./img.sh');
+
+    exec.on('exit', function() {
+        imgur.uploadFile('./animation.gif')
+            .then(function(json) {
+                socket.emit('complete', {
+                    clientId: clientId,
+                    url: json.data.link
+                });
+            })
+            .catch(function(err) {
+                console.error(err.message);
+            });
+    });
+
+    exec.stdout.on('data', function(data) {
+        if (data.indexOf('snap') > -1) {
+            socket.emit('snap', {
+                index: snapIndex,
+                clientId: clientId
+            });
+            snapIndex++;
+        }
+    });
+};
