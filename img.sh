d8ce710b45f19b3f4cca9eb912f3db4d594362c9
Chris Riebschlager <riebschlager@gmail.com>
2015-04-26 15:22:40 -0500

    back to jpg

--- img.sh
+++ img.sh
@@ -4,16 +4,16 @@ mkdir /home/pi/blvdia-camera/tmp
 
 for i in `seq 1 5`;
     do
-        fswebcam --no-banner -r 640x480 --png 9 --overlay /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/img$i.png
+        fswebcam --no-banner -r 640x480 --jpg 95 --overlay /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/img$i.jpg
         echo 'snap'
 done
 
 convert -delay 50 -size 640x480 \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img1.png \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img2.png \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img3.png \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img4.png \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img5.png \
+    -page +0+0 /home/pi/blvdia-camera/tmp/img1.jpg \
+    -page +0+0 /home/pi/blvdia-camera/tmp/img2.jpg \
+    -page +0+0 /home/pi/blvdia-camera/tmp/img3.jpg \
+    -page +0+0 /home/pi/blvdia-camera/tmp/img4.jpg \
+    -page +0+0 /home/pi/blvdia-camera/tmp/img5.jpg \
     -loop 0 /home/pi/blvdia-camera/animation.gif
 
 rm -rf /home/pi/blvdia-camera/tmp
