d92960c55fe6b7b56c70f92eaf350a4ceb0f9cf5
Chris Riebschlager <riebschlager@gmail.com>
2015-04-26 14:46:33 -0500

    order

--- img.sh
+++ img.sh
@@ -5,7 +5,7 @@ mkdir /home/pi/blvdia-camera/tmp
 for i in `seq 1 5`;
     do
         fswebcam --no-banner -r 640x480 --jpeg 95 /home/pi/blvdia-camera/tmp/img$i.jpg
-        composite -geometry  +0+0 /home/pi/blvdia-camera/tmp/img$i.jpg /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/timg$i.jpg
+        composite -geometry +0+0 /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/img$i.jpg /home/pi/blvdia-camera/tmp/timg$i.jpg
         echo 'snap'
 done
 
