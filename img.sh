085a22b2a2bc6695d1bbc48cf9b1d23154e866f4
Chris Riebschlager <riebschlager@gmail.com>
2015-05-10 21:34:52 -0500

    werd

--- img.sh
+++ img.sh
@@ -4,7 +4,7 @@ mkdir /home/pi/blvdia-camera/tmp
 
 for i in `seq 0 4`;
     do
-        fswebcam --no-banner -r 640x480 --jpeg 95 --overlay /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/img$i.jpg
+        fswebcam --no-banner -r 640x480 --overlay /home/pi/blvdia-camera/overlay.png /home/pi/blvdia-camera/tmp/img$i.jpg
         printf 'snap'
 done
 
