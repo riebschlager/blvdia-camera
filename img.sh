ab402212af492c0debd00bb6a1616d6ebba3e1e1
Chris Riebschlager <riebschlager@gmail.com>
2015-05-31 10:34:56 -0500

    simplify gif

--- img.sh
+++ img.sh
@@ -8,12 +8,14 @@ for i in `seq 0 4`;
         printf 'snap'
 done
 
-convert -delay 50 -size 640x480 \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img0.jpg \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img1.jpg \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img2.jpg \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img3.jpg \
-    -page +0+0 /home/pi/blvdia-camera/tmp/img4.jpg \
-    -loop 0 /home/pi/blvdia-camera/animation.gif
+# convert -delay 50 -size 640x480 \
+#     -page +0+0 /home/pi/blvdia-camera/tmp/img0.jpg \
+#     -page +0+0 /home/pi/blvdia-camera/tmp/img1.jpg \
+#     -page +0+0 /home/pi/blvdia-camera/tmp/img2.jpg \
+#     -page +0+0 /home/pi/blvdia-camera/tmp/img3.jpg \
+#     -page +0+0 /home/pi/blvdia-camera/tmp/img4.jpg \
+#     -loop 0 /home/pi/blvdia-camera/animation.gif
+
+convert -delay 50 -loop 0 /home/pi/blvdia-camera/tmp/*.jpg /home/pi/blvdia-camera/animation.gif
 
 rm -rf /home/pi/blvdia-camera/tmp
