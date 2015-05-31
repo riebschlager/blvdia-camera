a8ea9390e19a37fcf4d52a88c953e3650ff181f6
Chris Riebschlager <riebschlager@gmail.com>
2015-05-31 09:28:06 -0500

    grr

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function start(clientId) {
             Body: body
         }).
         send(function(err, data) {
-            console.log(data.location);
+            console.log(data);
             socket.emit('complete', {
                 clientId: clientId,
                 url: data.location
